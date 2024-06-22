ae5b4bcf9d29e13a8aa3a4d0a9a062589d5f8a49
Eduardo <eduardolustosa26@gmail.com>
2024-06-22 12:50:00 -0300

    initial config go project

--- src/repositories/images-repository.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma, Image } from '@prisma/client'
-
-export default interface ImagesRepository {
-  create(data: Prisma.ImageUncheckedCreateInput): Promise<Image>
-  findById(id: string): Promise<Image | null>
-  findByIdAndDelete(id: string): Promise<Image>
-}
